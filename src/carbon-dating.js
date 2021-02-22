69888e0fce7337408e961b1845e1b08b88acb526
Shoon <tishinadw@gmail.com>
2021-02-22 11:50:30 +0300

    feat: add carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,13 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(Number.isNaN(+sampleActivity) || typeof sampleActivity != 'string' || +sampleActivity <= 0 || +sampleActivity > MODERN_ACTIVITY){
+      return false;
+  }
+  
+   let res = Math.log(MODERN_ACTIVITY / +sampleActivity) / (Math.LN2 / HALF_LIFE_PERIOD);
+   return Math.ceil(res);   
 };
+
+
